1c60b43e23d6afada70cdf546a8541fb24079c3a
黄明富 <212149997@qq.com>
2024-11-14 15:56:36 +0800

    update

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config(); // 加载.env环境变量
+
 const express = require("express");
 const app = express();
 const articlesRoutes = require("./articlesRoutes"); // 引入文章路由模块
@@ -8,7 +10,7 @@ app.use("/articles", articlesRoutes); // 使用文章路由
 // 如果不指定端口号，服务器将无法启动
 // 生产环境：可以使用环境变量来控制端口号，这样部署时可以灵活配置。
 //如果需要使用标准端口（80 或 443），通常会在前端有一个反向代理（如 Nginx 或 Apache），它监听标准端口并将请求转发到 Node.js 应用的非标准端口。
-let port = process.env.PORT || 3000; // 初始端口号
+let port = process.env.PORT; // 初始端口号
 
 function startServer(port) {
   app
