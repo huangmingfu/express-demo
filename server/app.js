8e54b0f33c64a95ff2efb1ccb244665d805b0e2c
黄明富 <212149997@qq.com>
2024-11-14 15:44:29 +0800

    update

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const articlesRoutes = require("./articlesRoutes"); // 引入文章路由模块
 app.use(express.json());
 app.use("/articles", articlesRoutes); // 使用文章路由
 
+// 如果不指定端口号，服务器将无法启动
+// 生产环境：可以使用环境变量来控制端口号，这样部署时可以灵活配置。
+//如果需要使用标准端口（80 或 443），通常会在前端有一个反向代理（如 Nginx 或 Apache），它监听标准端口并将请求转发到 Node.js 应用的非标准端口。
 let port = process.env.PORT || 3000; // 初始端口号
 
 function startServer(port) {
