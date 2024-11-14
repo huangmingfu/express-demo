d98d48d66b930b7c29e99a1d2d9322073abf3b49
黄明富 <212149997@qq.com>
2024-11-14 11:44:59 +0800

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div
+      class="p-4 bg-blue-500 text-white font-bold rounded-lg shadow-lg hover:bg-blue-600"
+    >
+      Hello Tailwind CSS!
+    </div>
     <ArticlesPage />
   </div>
 </template>
